8eb1e82432c40b967d0fbf95bddefd92bd3a86ae
Nim <91138609+NimUsagi@users.noreply.github.com>
2022-06-23 17:20:30 -0300

    Delete pacote_yasmin directory

--- pacote_yasmin/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='nim_irgalaxies',
-      version='0.0.2',
-      author='Yasmin',
-      author_email='yasmincfcoelho@gmail.com',
-      license='Creative Commons Attribution 4.0 International License',
-      packages=['nim_irgalaxies'],
-      zip_safe=False)
